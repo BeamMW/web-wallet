e41aabd533bc3aa6a90c1db84acff3d381543474
vkozulya <kozulyav@gmail.com>
2021-08-31 13:29:58 +0300

    effector model cleanup

--- src/app/pages/main/send/model.ts
+++ src/app/pages/main/send/model.ts
@@ -1,6 +1,6 @@
 import { $balance } from '@state/portfolio';
 import {
-  combine, createEvent, restore, sample,
+  combine, createEffect, createEvent, restore, sample,
 } from 'effector';
 import { debounce } from 'patronum/debounce';
 
@@ -10,54 +10,51 @@ import React from 'react';
 
 export const setSelected = createEvent<number>();
 
-export const $selected = restore(setSelected, 0);
-
 export const $options = $balance.map((state) => (
   state.map(({ name }) => name)
 ));
-
-export const $asset = combine($balance, $selected, (array, index) => array[index]);
 
 interface ChangeEvent extends React.ChangeEvent<HTMLInputElement> {}
 
 export const onAmountInput = createEvent<ChangeEvent>();
 
-const setAmountSafe = onAmountInput.map(({ target }) => target.value);
 const setAmount = createEvent<string>();
 
 const REG_AMOUNT = /^(\d+\.?)?(\d+)?$/;
+
+const setAmountSafe = onAmountInput
+  .map(({ target }) => target.value)
+  .filter({
+    fn: (value) => REG_AMOUNT.test(value),
+  });
+
+export const $selected = restore(setSelected, 0);
+export const $asset = combine($balance, $selected, (array, index) => array[index]);
+export const $amount = restore(setAmount, '');
 
 sample({
   source: $asset,
   clock: setAmountSafe,
   fn: ({ available }, value) => {
-    if (!REG_AMOUNT.test(value)) {
-      return;
-    }
-
     const total = available / GROTHS_IN_BEAM;
-
-    setAmount(
-      parseFloat(value) > total
-        ? total.toString() : value,
-    );
+    return parseFloat(value) > total
+      ? total.toString() : value;
   },
+  target: setAmount,
 });
 
-export const $amount = restore(setAmount, '');
-
 const setAmountDebounced = debounce({ source: setAmount, timeout: 200 });
+
+const calculateChangeFx = createEffect((params) => calculateChange(params));
 
 sample({
   source: $asset,
   clock: setAmountDebounced,
-  fn: ({ asset_id }, payload) => {
-    const amount = parseFloat(payload) * GROTHS_IN_BEAM;
-    calculateChange({
-      amount,
-      asset_id,
-      fee: FEE_DEFAULT,
-      is_push_transaction: false,
-    });
-  },
+  fn: ({ asset_id }, payload) => ({
+    asset_id,
+    amount: parseFloat(payload) * GROTHS_IN_BEAM,
+    fee: FEE_DEFAULT,
+    is_push_transaction: false,
+  }),
+  target: calculateChangeFx,
 });
