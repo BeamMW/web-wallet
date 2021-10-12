4346fd4f3264865d37914268a1321c7066f84d75
anatol <anatol@beam-mw.com>
2021-10-12 17:01:47 +0300

    #54 updated target  version

--- update_version.sh
+++ update_version.sh
@@ -6,10 +6,25 @@ MANIFEST="src/manifest.json"
 REVISION=$(git rev-list HEAD --count)
 
 if [[ $OUTPUT =~ $REGEX ]]; then
+	echo -n "${BASH_REMATCH[2]}.$REVISION" > version.gen
 	sed "s/%BEAM_VERSION%/${BASH_REMATCH[2]}.$REVISION/" $MANIFEST.in > $MANIFEST.tmp;
 fi
 
 NETWORK=${BASH_REMATCH[1]}
+case $NETWORK in
+	"testnet")
+		echo -n "Testnet" > network.gen
+	;;
+	"")
+		echo -n "Mainnet" > network.gen
+	;;
+	*)
+		echo -n "Masternet" > network.gen		
+	;;
+
+esac
+
+
 
 if [[ $NETWORK != '' ]]; then
 	sed "s/%BEAM_NETWORK%/ ($NETWORK)/" $MANIFEST.tmp > $MANIFEST;
