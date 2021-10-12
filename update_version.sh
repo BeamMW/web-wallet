2a77e5433ba4cf83b1212f9e6a6f64a625ccadfa
anatol <anatol@beam-mw.com>
2021-10-12 17:16:37 +0300

    #54 unified artifact name

--- update_version.sh
+++ update_version.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-REGEX="beam-wasm-client\-?([a-z]*)@([0-9]+\.[0-9]+\.[0-9]+)"
+REGEX="beam-wasm-client(\-?([a-z]*))@([0-9]+\.[0-9]+\.[0-9]+)"
 OUTPUT=$(npm list | grep beam-wasm-client*)
 MANIFEST="src/manifest.json"
 REVISION=$(git rev-list HEAD --count)
 
 if [[ $OUTPUT =~ $REGEX ]]; then
-	echo -n "${BASH_REMATCH[2]}.$REVISION" > version.gen
-	sed "s/%BEAM_VERSION%/${BASH_REMATCH[2]}.$REVISION/" $MANIFEST.in > $MANIFEST.tmp;
+	VERSION="${BASH_REMATCH[3]}.$REVISION"
+	echo -n $VERSION > version.gen
+	sed "s/%BEAM_VERSION%/$VERSION/" $MANIFEST.in > $MANIFEST.tmp;
 fi
 
-NETWORK=${BASH_REMATCH[1]}
-case $NETWORK in
-	"testnet")
-		echo -n "Testnet" > network.gen
-	;;
-	"")
-		echo -n "Mainnet" > network.gen
-	;;
-	*)
-		echo -n "Masternet" > network.gen		
-	;;
-
-esac
-
-
+SUFFIX=${BASH_REMATCH[1]}
+NETWORK=${BASH_REMATCH[2]}
+echo -n $SUFFIX > suffix.gen
 
 if [[ $NETWORK != '' ]]; then
-	sed "s/%BEAM_NETWORK%/ ($NETWORK)/" $MANIFEST.tmp > $MANIFEST;
+	NETWORK=" ($NETWORK)"
 fi
+
+sed "s/%BEAM_NETWORK%/$NETWORK/" $MANIFEST.tmp > $MANIFEST;
 
 rm $MANIFEST.tmp
