bfdf945f03a474df026e26b3ae1e92c6b83f2f6f
Denis Stetskov <botozavreg@gmail.com>
2021-11-18 15:48:54 +0200

    redux debugger midelware added

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-const path = require('path');
-const { IgnorePlugin } = require('webpack');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require("path");
+const { IgnorePlugin } = require("webpack");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-const IN_DEVELOPMENT = process.env.NODE_ENV !== 'production';
+const IN_DEVELOPMENT = process.env.NODE_ENV !== "production";
 
 const config = {
-  target: 'web',
+  target: "web",
   cache: false,
   entry: {
-    index: path.join(__dirname, './src/index.tsx'),
-    background: path.join(__dirname, './src/background.ts'),
-    contentscript: path.join(__dirname, './src/contentscript.ts'),
-    inpage: path.join(__dirname, './src/inpage.ts'),
+    index: path.join(__dirname, "./src/index.tsx"),
+    background: path.join(__dirname, "./src/background.ts"),
+    contentscript: path.join(__dirname, "./src/contentscript.ts"),
+    inpage: path.join(__dirname, "./src/inpage.ts"),
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
     clean: true,
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
     plugins: [new TsconfigPathsPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['babel-loader', '@linaria/webpack-loader'],
+        use: ["babel-loader", "@linaria/webpack-loader"],
         exclude: /node_modules/,
       },
       {
         test: /\.svg$/,
-        use: ['@svgr/webpack', 'svgo-loader'],
+        use: ["@svgr/webpack", "svgo-loader"],
       },
       {
         test: /\.css$/,
@@ -43,7 +43,7 @@ const config = {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               url: false,
               sourceMap: IN_DEVELOPMENT,
@@ -58,58 +58,58 @@ const config = {
       resourceRegExp: /^ws$/,
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: "styles.css",
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.join(__dirname, './node_modules/beam-wasm-client-masternet/'),
+          from: path.join(__dirname, "./node_modules/beam-wasm-client-masternet/"),
           globOptions: {
-            ignore: ['package.json'],
+            ignore: ["package.json"],
           },
         },
         {
-          from: path.join(__dirname, 'src/assets'),
-          to: path.join(__dirname, 'dist/assets'),
-          context: 'public',
+          from: path.join(__dirname, "src/assets"),
+          to: path.join(__dirname, "dist/assets"),
+          context: "public",
         },
         {
-          from: path.join(__dirname, 'src/page.html'),
-          to: path.join(__dirname, 'dist'),
-          context: 'public',
+          from: path.join(__dirname, "src/page.html"),
+          to: path.join(__dirname, "dist"),
+          context: "public",
         },
         {
-          from: path.join(__dirname, 'src/notification.html'),
-          to: path.join(__dirname, 'dist'),
-          context: 'public',
+          from: path.join(__dirname, "src/notification.html"),
+          to: path.join(__dirname, "dist"),
+          context: "public",
         },
         {
-          from: path.join(__dirname, 'src/popup.html'),
-          to: path.join(__dirname, 'dist'),
-          context: 'public',
+          from: path.join(__dirname, "src/popup.html"),
+          to: path.join(__dirname, "dist"),
+          context: "public",
         },
         {
-          from: path.join(__dirname, 'src/background.html'),
-          to: path.join(__dirname, 'dist'),
-          context: 'public',
+          from: path.join(__dirname, "src/background.html"),
+          to: path.join(__dirname, "dist"),
+          context: "public",
         },
         {
-          from: path.join(__dirname, 'src/manifest.json'),
-          to: path.join(__dirname, 'dist/manifest.json'),
-          context: 'public',
+          from: path.join(__dirname, "src/manifest.json"),
+          to: path.join(__dirname, "dist/manifest.json"),
+          context: "public",
         },
       ],
     }),
     new webpack.ProvidePlugin({
-      process: 'process/browser',
+      process: "process/browser",
     }),
   ],
-  externals: ['fs'],
+  externals: ["fs"],
 };
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    config.devtool = 'inline-source-map';
+  if (argv.mode === "development") {
+    config.devtool = "inline-source-map";
   }
 
   return config;
