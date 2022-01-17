8eb5c593ac45a7b959d6f240325b70abcf643aa1
Denis Stetskov <botozavreg@gmail.com>
2022-01-17 16:25:08 +0200

    address label fixes

--- src/app/containers/Wallet/containers/Send/SendForm.tsx
+++ src/app/containers/Wallet/containers/Send/SendForm.tsx
@@ -77,7 +77,7 @@ const validate = async (values: SendFormData, setHint: (string) => void) => {
   }
 
   if (values.offline) {
-    const warning = addressData.payments === 1
+    const warning = addressData.payments > 1
       ? 'transactions left.'
       : 'transaction left. Ask receiver to come online to support more offline transaction.';
 
